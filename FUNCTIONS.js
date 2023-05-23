10c93206978d04e5a5f45862b52724995932dd89
Tuukka Pitkänen <tuksu090@hotmail.com>
2023-05-23 11:36:30 +0300

    style

--- FUNCTIONS.js
+++ FUNCTIONS.js
@@ -50,12 +50,53 @@ export function getHtml(htmlName){
         return `
         <div>
             <p id="currentView" style="visibility: hidden">votingView</p>
-            <h1>votingView</h1>
             <div id="peopleToVote"></div>
+            <button id="sendMessageButton" class="btn" onclick="">Vahvista valinta</button>
+            <p id="timer"></p>
         </div>
     `
     }else if (htmlName == "chatView"){
         return `
+        <section class="messenger">
+        <div class="messenger-header">
+            <h1 style="position: relative;" id="kysymys">Kysymyksiä Firebasesta</h1><br>
+            <p id="timer"></p>
+        </div>
+        <!-- Start of the Messages -->
+        <main class="messenger-chat">
+            <!-- First Left Message AI -->
+            <div class="message left-message">
+                <div class="message-img" style="background-image: url(https://cdn-icons-png.flaticon.com/512/773/773330.png);"></div>
+                <div class="message-bubble">
+                    <div class="message-info">
+                        <div class="message-info-name" id="ai_username"></div>
+                        <div class="message-info-time">00:00</div>
+                    </div>
+                    <!-- id="response" että AI ymmärtää sinne sen laittaa -->
+                    <div class="message-text" id="response1" >
+                    </div>
+                </div>
+            </div>
+            <!-- First Right Message Player -->
+            <div class="message right-message">
+                <div class="message-img" style="background-image: url(https://freesvg.org/img/abstract-user-flat-4.png);"></div>
+                <div class="message-bubble">
+                    <div class="message-info">
+                        <div class="message-info-name" id="username"></div>
+                        <div class="message-info-time">00:00</div>
+                    </div>
+                    <div class="message-text">
+                        >Oma Vastaus<
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!-- Oma vastaus -->
+        <form class="messenger-inputarea">
+            <input id="sendMessageInput" maxlength="10" type="text" class="messenger-input" placeholder="Kirjoita vastauksesi...">
+            <button id="sendMessageButton" type="button" class="messenger-send-btn" onclick=""><i class="bi bi-send"></i></button>
+        </form>
+        </section>
             <div>
                 <p id="currentView" style="visibility: hidden">chatView</p>
                 <div class="container">
@@ -84,4 +125,3 @@ export function getHtml(htmlName){
     to see snapshot value do snapshot.val()
 
 */
-
